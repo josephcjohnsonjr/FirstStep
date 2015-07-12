4bf66f7d3b2a865037a5bc5a4f9b7beabe147eb5
Joseph Johnson <josephcjohnsonjr@gmail.com>
2015-07-12 02:15:23 -0700

    getting console data

--- controllers/api.js
+++ controllers/api.js
@@ -60,7 +60,8 @@ exports.getGithub = function(req, res, next) {
 };
 
 exports.postGithubWebhook = function(req, res, next) { //set up github webhook => https://developer.github.com/v3/repos/hooks/#create-a-hook
-  res.send(req);
+  console.log(JSON.strigify(req));
+  res.send('hook recieved');
   /*var token = _.find(req.user.tokens, { kind: 'github' });
   var github = new Github({ token: token.accessToken });
   var repo = github.getRepo('sahat', 'requirejs-library');
